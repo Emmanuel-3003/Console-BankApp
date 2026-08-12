1059bdcacdf31feea6e0e457488dd273b06baf5e
Emmanuel Gabriel <degabriels03@gmail.com>
2026-08-12 10:51:32 +0530

    function to open an account

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,15 @@
 package app;
 
+import service.BankService;
+import service.impl.BankServiceImpl;
+
 import java.util.Scanner;
 
 public class Main {
     static void main(String[] args) {
 
         Scanner sc =  new Scanner(System.in);
+        BankService bankService = new BankServiceImpl();
 
         boolean running = true;
         System.out.println("Welcome to Console Bank..");
@@ -26,9 +30,52 @@ public class Main {
             System.out.println("CHOICE : " + choice);
 
             switch (choice){
+                case "1" -> openAccount(sc, bankService);
+                case "2" -> deposit(sc);
+                case "3" -> withdraw(sc);
+                case "4" -> transfer(sc);
+                case "5" -> statement(sc);
+                case "6" -> listAccount(sc);
+                case "7" -> searchAccount(sc);
                 case "0" -> running = false;
 
             }
         }
+    }
+
+    private static void openAccount(Scanner sc, BankService bankService) {
+        System.out.println("Customer Name : ");
+        String name = sc.nextLine().trim();
+
+        System.out.println("Customer Email : ");
+        String email = sc.nextLine().trim();
+
+        System.out.println("Account Type(CURRENT/SAVINGS) : ");
+        String type = sc.nextLine().trim();
+
+        System.out.println("Initial Deposit(OPTIONAL) : ");
+        String amountStr = sc.nextLine().trim();
+        Double initial = Double.valueOf(amountStr);
+
+        bankService.openAccount(name, email, type);
+    }
+
+    private static void deposit(Scanner sc) {
+        
+    }
+
+    private static void withdraw(Scanner sc) {
+    }
+
+    private static void transfer(Scanner sc) {
+    }
+
+    private static void statement(Scanner sc) {
+    }
+
+    private static void listAccount(Scanner sc) {
+    }
+
+    private static void searchAccount(Scanner sc) {
     }
 }
---
+++ src/service/impl/BankServiceImpl.java
@@ -0,0 +1,16 @@
+package service.impl;
+
+import domain.Account;
+import service.BankService;
+
+import java.util.UUID;
+
+public class BankServiceImpl implements BankService {
+    @Override
+    public String openAccount(String name, String email, String accountType) {
+        String customerId = UUID.randomUUID().toString();
+        String accountNumber = UUID.randomUUID().toString();
+        Account a = new Account(accountNumber, accountType, (double) 0, customerId);
+        return "";
+    }
+}
