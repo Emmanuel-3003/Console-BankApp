a2ebf1068ad60d09ba1eef2679208909898a3f77
Emmanuel Gabriel <degabriels03@gmail.com>
2026-08-13 10:45:02 +0530

    withdrawal fucntion

--- src/service/impl/BankServiceImpl.java
+++ src/service/impl/BankServiceImpl.java
@@ -47,6 +47,22 @@ public class BankServiceImpl implements BankService {
         transactionRepository.add(transaction);
     }
 
+    @Override
+    public void withdraw(String accountNumber, Double amount, String note) {
+        Account account = accountRepository.findByNumber(accountNumber)
+                .orElseThrow(() -> new RuntimeException("Account not found : " + accountNumber));
+
+        if(account.getBalance() < amount) {
+            throw new RuntimeException("Insufficient funds");
+        }
+
+        account.setBalance(account.getBalance() - amount);
+
+        Transaction transaction = new Transaction(UUID.randomUUID().toString(), Type.WITHDRAW, account.getAccountNumber(),
+                amount, LocalDateTime.now(), note);
+        transactionRepository.add(transaction);
+    }
+
     private String getAccountNumber() {
         int size = accountRepository.findAll().size()+1;
         return String.format("AC%06d", size);
